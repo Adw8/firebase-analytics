52d1301d8299e0c361989b09f3820788d79007d5
Adwait Gharpure <adwaitgharpure@gmail.com>
2023-12-22 19:51:36 +0530

    Added protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Signup from './components/Signup'
 import Account from './components/Account'
 import { Route, Routes } from 'react-router-dom'
 import { AuthContextProvider } from './context/AuthContext'
+import ProtectedRoute from './components/ProtectedRoute'
 
 
 function App() {
@@ -20,8 +21,9 @@ function App() {
       <Routes>
         <Route path='/' element={<Signin />} />
         <Route path='/signup' element={<Signup />} />
-        <Route path='/account' element={<Account />} />
-
+        <Route path='/account' element={
+        <ProtectedRoute><Account /></ProtectedRoute>
+        } />
         </Routes>
         </AuthContextProvider>
     </>
